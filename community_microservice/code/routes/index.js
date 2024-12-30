6b4b520ba66039026bc99028a629d37e8f5b73da
Anan Li <li0047@hz.nl>
2024-12-30 15:39:43 +0800

    update community routes

--- community_microservice/code/routes/index.js
+++ community_microservice/code/routes/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllCommunites, getCommunity, joinCommunity, createCommunity, storeCommunity, updateCommunity, deleteCommunity, getMemberRole } from '../controllers/controller.js';
+import { getAllCommunites, getCommunity, joinCommunity, createCommunity, storeCommunity, updateCommunity, deleteCommunity, getMemberRole, leaveCommunity } from '../controllers/controller.js';
 import { getCommunities, getCommunityMembers, deleteCommunityById, runSeeds} from './dbManager.js';
 
 const router = express.Router();
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get('/', getAllCommunites);
 router.get('/:id', getCommunity);
 router.post('/join/:community_id', joinCommunity);
+router.post('/leave/:community_id', leaveCommunity)
 router.post('/member-role/:community_id', getMemberRole);
 router.get('/create', createCommunity);
 router.post('/create', storeCommunity);
