8f28ad997faf54e1f73628843761725dd76cc673
Frederic Borowiak <frenibo@gmail.com>
2025-01-07 15:27:20 +0100

    hotfix item_service

--- item_microservice/code/routes/index.js
+++ item_microservice/code/routes/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllItems, getItem, storeItem, updateItem, deleteItem, getItemsByCommunityId } from '../controllers/controller.js';
+import { getAllItems, getItem, storeItem, updateItem, deleteItem } from '../controllers/controller.js';
 
 const router = express.Router();
 
